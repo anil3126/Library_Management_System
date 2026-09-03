ccee67dd4d2a3fc327f47489bb42babcb5a93d9f
ANIL <anilgangavath275@gmail.com>
2026-09-03 22:36:00 +0530

    book management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import { ToastContainer } from "react-toastify"
 import { useDispatch, useSelector } from "react-redux";
 import { getUser } from "./store/slices/authSlice.js";
 import { fetchAllUsers } from "./store/slices/userSlice.js";
+import { fetchAllBooks } from "./store/slices/bookSlice.js";
 
 const App = () => {
 
@@ -18,6 +19,7 @@ const App = () => {
 
   useEffect(()=>{
     dispatch(getUser());
+    dispatch(fetchAllBooks())
 
     if(isAuthenticated && user?.role === "Admin"){
       dispatch(fetchAllUsers())
--- client/src/components/BookManagement.jsx
+++ client/src/components/BookManagement.jsx
@@ -5,20 +5,25 @@ import { toggleAddBookPopup, toggleReadBookPopup, toggleRecordBookPopup } from "
 import {toast} from "react-toastify"
 import { fetchAllBooks, resetBookSlice } from "../store/slices/bookSlice";
 import { fetchAllBorrowedBooks, resetBorrowSlice } from "../store/slices/borrowSlice";
-import Header from "../layout/Header" 
+import Header from "../layout/Header"
+import AddBookPopup from "../popups/AddBookPopup"
+import ReadBookPopup from "../popups/ReadBookPopup"
+import RecordBookPopup from "../popups/RecordBookPopup"
 
 const BookManagement = () => {
     
   const dispatch = useDispatch();
 
   const {loading,error, message, books} =useSelector((state)=> state.book);
-  const {isAuthenticated, user} = useSelector((state)=> state.user);
+  const {isAuthenticated, user} = useSelector((state)=> state.auth);
+  
+  
 
   
   const { 
         addBookPopup,
         readBookPopup,
-        recordBookPopup,
+        recordBookPopup
          } = useSelector((state)=> state.popup)
 
   const {
@@ -30,7 +35,7 @@ const BookManagement = () => {
   const [readBook, setReadBook] = useState({});
 
   const openReadPopup = (id) =>{
-    const book = books.find((book)=>book._id === id);
+    const book = books.find((book) => book._id === id);
     setReadBook(book);
     dispatch(toggleReadBookPopup());
   };
@@ -69,13 +74,15 @@ const BookManagement = () => {
     }
     const searchedBooks = books.filter((book) => 
         book.title.toLowerCase().includes(searchedKeyword)
-    )
+)
     
-
+  
   return (
     <>
         <main className="relative flex-1 p-6 pt-28">
           <Header/>
+
+            {/* Sub Header */}
           <header className="flex flex-col gap-3 md:flex-row md:justify-between md:items-center">
               <h2 className="text-xl font-medium md:text-2xl md:font-semibold">
                 {user && user.role === "Admin" ? "Book Management" : "Books"}
@@ -83,7 +90,7 @@ const BookManagement = () => {
                 
               <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4">
                  {isAuthenticated && user?.role === "Admin" && (
-                    <button onClick={()=> dispatch(toggleAddBookPopup())}
+                    <button onClick={() => dispatch(toggleAddBookPopup())}
                         className="relative pl-14 w-full sm:w-52 flex gap-4 justify-center items-center py-2 px-4
                         bg-black text-white rounded-md hover:bg-gray-800"
                     >
@@ -98,11 +105,82 @@ const BookManagement = () => {
                       </button>
                   )
                  }
+
+                 <input 
+                      type="text" 
+                      placeholder="Search books.." 
+                      className="w-full sm:w-52 border p-2 border-gray-300 rounded-md"
+                      value={searchedKeyword}
+                      onChange={handleSearch}
+                      />
               </div>
 
-        </header>
+          </header>
+
+            {/* Table */}
+            {
+              books && books.length > 0 ? (
+                <div className="mt-6 overflow-auto bg-white rounded-md shadow-lg">
+                  <table className="min-w-full border-collaps">
+                    <thead>
+                      <tr className="bg-gray-200">
+                        <th className="px-4 py-2 text-left">ID</th>
+                        <th className="px-4 py-2 text-left">Name</th>
+                        <th className="px-4 py-2 text-left">Author</th>
+
+                        {
+                          isAuthenticated && user?.role === "Admin" && (
+                            <th className="px-4 py-2 text-left">Quantity</th>
+                        )}
+                        
+                        <th className="px-4 py-2 text-left">Price</th>
+                        <th className="px-4 py-2 text-left">Availability</th>
+                         {
+                          isAuthenticated && user?.role === "Admin" && (
+                            <th className="px-4 py-2 text-center">Record Book</th>
+                        )}
+
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      {
+                        searchedBooks.map((book, index) =>(
+                          <tr key={book._id}
+                              className={(index+1) % 2 === 0 ? "bg-gray-50" : ""}
+                             >
+                              <td className="px-4 py-2">{index+1}</td>
+                              <td className="px-4 py-2">{book.title}</td>
+                              <td className="px-4 py-2">{book.author}</td>
+                              {isAuthenticated && user?.role === "Admin" && (
+                               <td className="px-4 py-2 text-left">{book.quantity}</td>
+                               )}
+                               <td className="px-4 py-2">{`$${book.price}`}</td>
+                               <td className="px-4 py-2">{book.availability ? "Available" : "Unavailable"}</td>
+                              {isAuthenticated && user?.role === "Admin" && (
+                               <td className="px-4 py-2 flex space-x-2 my-3 justify-center">
+                                <BookA onClick={()=> openReadPopup(book._id)} />
+                                  <NotebookPen onClick={() => openRecordBookPopup(book._id)} />
+                               </td>
+                               )}
+                             </tr>
+                        )) 
+                      }
+                    </tbody>
+                  </table>
+                </div>
+              ) : (
+                <h3 className="text-3xl mt-5 font-medium">
+                  No books found in library!
+                  </h3>
+              )
+            }
 
         </main>
+
+        {addBookPopup && <AddBookPopup/>}
+        {readBookPopup && <ReadBookPopup book={readBook}/>}
+        {recordBookPopup && <RecordBookPopup bookId={borrowBookId}/>}
   </>
   );
 };
