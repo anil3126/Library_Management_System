aeae55169ba2cde3136216a09c1ab8e4d6ed8c89
ANIL <anilgangavath275@gmail.com>
2026-09-25 14:10:16 +0530

    updated changesss

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,24 +1,25 @@
 import nodemailer from "nodemailer";
 
-export const sendEmail = async ({email, subject, message}) => {
+export const sendEmail = async ({ email, subject, message }) => {
 
     console.log("SMTP MAIL:", process.env.SMTP_MAIL);
-    console.log("SMTP PASSWORD EXISTS:", !!process.env.SMTP_PASSWORD);
+    console.log(
+        "SMTP PASSWORD EXISTS:",
+        !!process.env.SMTP_PASSWORD
+    );
 
     const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        service: process.env.SMTP_SERVICE,
-        port: process.env.SMTP_PORT,
+        host: "smtp.gmail.com",
+        port: 465,
         secure: true,
         auth: {
             user: process.env.SMTP_MAIL,
-            pass: process.env.SMTP_PASSWORD
+            pass: process.env.SMTP_PASSWORD,
         },
-         tls: {
-            rejectUnauthorized: false
-        }
-
     });
+
+    // Actually verify SMTP connection
+    await transporter.verify();
 
     console.log("SMTP CONNECTION SUCCESS");
 
@@ -26,12 +27,12 @@ export const sendEmail = async ({email, subject, message}) => {
         from: process.env.SMTP_MAIL,
         to: email,
         subject: subject,
-        html: message
-    }
+        html: message,
+    };
 
- const info = await transporter.sendMail(mailOptions);
+    const info = await transporter.sendMail(mailOptions);
 
-  console.log("EMAIL SENT:", info.messageId);
+    console.log("EMAIL SENT:", info.messageId);
 
-  return info;
-}
+    return info;
+};
